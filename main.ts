bdf2d895b666b7cde20a98a01dba7cf4bfc98cfe
ittk1229 <s2010485@u.tsukuba.ac.jp>
2024-11-17 02:16:47 +0900

    Implement features described in README

--- main.ts
+++ main.ts
@@ -1,14 +1,26 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { link } from "fs";
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
-
+// 置換先のフィールド名，フロントマターをコピーするかどうか
 interface MyPluginSettings {
-	mySetting: string;
+	fieldName: string;
+	includeFrontmatter: boolean;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	fieldName: "url",
+	includeFrontmatter: false,
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -16,94 +28,155 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: "externalize-selected-text",
+			name: "Externalize selected text",
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				const selectedText = editor.getSelection();
+				const externalLinks = this.getExternalLinks();
+				const externalizedText = this.externalizeText(
+					selectedText,
+					externalLinks
+				);
+				navigator.clipboard.writeText(externalizedText);
+				new Notice("Externalized text was copied to clipboard!");
+			},
 		});
+
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: "externalize-whole-page",
+			name: "Externalize whole page",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+				let wholePageText = view.data;
+				if (!this.settings.includeFrontmatter) {
+					wholePageText = wholePageText.replace(
+						/---\n(.|\n)*?\n---\n/,
+						""
+					);
+				}
+				const externalLinks = this.getExternalLinks();
+				const externalizedText = this.externalizeText(
+					wholePageText,
+					externalLinks
+				);
+				navigator.clipboard.writeText(externalizedText);
+				new Notice("Externalized page was copied to clipboard!");
+			},
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		// This creates an icon in the left ribbon.
+		this.addRibbonIcon(
+			"bracket-glyph",
+			"Link Externalizer",
+			(evt: MouseEvent) => {
+				const editor =
+					this.app.workspace.getActiveViewOfType(
+						MarkdownView
+					)?.editor;
+				if (!editor) return;
+
+				// 選択範囲があるか確認
+				const selectedText = editor.getSelection();
+				if (selectedText) {
+					// 選択範囲がある場合、'externalize-selected-text' コマンドを実行
+					this.app.commands.executeCommandById(
+						"obsidian-link-externalizer:externalize-selected-text"
+					);
+				} else {
+					// 選択範囲がない場合、'externalize-whole-page' コマンドを実行
+					this.app.commands.executeCommandById(
+						"obsidian-link-externalizer:externalize-whole-page"
+					);
 				}
 			}
-		});
+		);
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	private getExternalLinks() {
+		const activeFile = this.app.workspace.getActiveFile();
+		if (!activeFile) {
+			new Notice("No active file.");
+			return;
+		}
 
+		const activeFileCache = this.app.metadataCache.getFileCache(activeFile);
+		if (!activeFileCache || !activeFileCache.links) {
+			new Notice("No links found in the active file.");
+			return;
+		}
+
+		const externalLinks = {};
+
+		activeFileCache.links.forEach((link) => {
+			const fileName = link.link.split(/[#^|]/)[0];
+
+			if (externalLinks[fileName]) return;
+			externalLinks[fileName] = null;
+
+			const linkedFile = this.app.metadataCache.getFirstLinkpathDest(
+				fileName,
+				"/"
+			);
+			if (!linkedFile) {
+				console.log(link.link, "1. No linked file.");
+				return;
+			}
+			// ファイルを取得
+			const linkedFileCache =
+				this.app.metadataCache.getFileCache(linkedFile);
+			if (!linkedFileCache) {
+				console.log(link.link, "2. No linked file cache.");
+				return;
+			}
+			// ファイルの frontmatter を取得
+			if (!linkedFileCache.frontmatter) {
+				console.log(link.link, "3. No frontmatter found.");
+				return;
+			}
+			const frontmatter = linkedFileCache.frontmatter;
+			// frontmatter から url を取得
+			if (!frontmatter.url) {
+				console.log(link.link, "4. No url found.");
+				return;
+			}
+			const url = frontmatter[this.settings.fieldName];
+
+			externalLinks[fileName] = url;
+		});
+
+		return externalLinks;
+	}
+
+	private externalizeText(text: string, externalLinks: object) {
+		const result = text.replace(/\[\[(.+?)\]\]/g, (match, p1) => {
+			// #: link to heading
+			// ^: link to block
+			// |: alias
+			const [linkText, alias] = p1.split("|");
+			const fileName = linkText.split(/[#^]/)[0];
+			const displayText = alias || linkText;
+			if (!externalLinks[fileName]) return displayText;
+			return `[${displayText}](${externalLinks[fileName]})`;
+		});
+
+		return result;
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -116,19 +189,21 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Field name")
+			.setDesc("field name to be used as url")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.fieldName)
+					.onChange(async (value) => {
+						this.plugin.settings.fieldName = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
